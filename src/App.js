3502d1d8cda07b00b7e97bb40d89f26d78d21bc1
Bukilola <Arasanyin01@gmail.com>
2023-01-10 18:34:59 +0100

    Added the github url

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import "./App.css";
 
-import './App.css';
-
-
-export default  function App() {
+export default function App() {
   return (
     <div className="App">
-      Weather App
-      </div>
+      <h1>Weather App</h1>
+      <footer>
+        This project was created by {""}
+        <a href="https://bukeysblog.wordpress.com/" target="-blank">
+          {" "}
+          Arasanyin Olubukola
+        </a>{" "}
+        and it is{" "}
+        <a href="https://github.com/Bukilola/react-weather-app" target="-blank">
+          {" "}
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
   );
 }
-
