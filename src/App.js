4efaf5057dea4f44e2bbecfd074e74e5c2734012
Bukilola <Arasanyin01@gmail.com>
2023-01-11 21:15:30 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,19 +3,25 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was created by {""}
-        <a href="https://bukeysblog.wordpress.com/" target="-blank">
-          {" "}
-          Arasanyin Olubukola
-        </a>{" "}
-        and it is{" "}
-        <a href="https://github.com/Bukilola/react-weather-app" target="-blank">
-          {" "}
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div classname="container">
+        <h1>Weather App</h1>
+
+        <footer>
+          This project was created by {""}
+          <a href="https://bukeysblog.wordpress.com/" target="-blank">
+            {" "}
+            Arasanyin Olubukola
+          </a>{" "}
+          and it is{" "}
+          <a
+            href="https://github.com/Bukilola/react-weather-app"
+            target="-blank"
+          >
+            {" "}
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
