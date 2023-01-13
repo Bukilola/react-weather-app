e6f54a21c23307e231ee35b813ed7ff084ce20e1
Bukilola <Arasanyin01@gmail.com>
2023-01-13 18:29:25 +0100

    Redoing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import "./App.css";
-import Weather from "./Weather"
+import SearchWeather from "./Weather"
 export default function App() {
   return (
     <div className="App">
       <div classname="container">
-    
-<Weather />
-        <footer>
-          This project was created by {""}
-          <a href="https://bukeysblog.wordpress.com/" target="-blank">
-            {" "}
-            Arasanyin Olubukola
-          </a>{" "}
-          and it is{" "}
-          <a
-            href="https://github.com/Bukilola/react-weather-app"
-            target="-blank"
-          >
-            {" "}
-            open-sourced on GitHub
-          </a>
-        </footer>
+        <SearchWeather defaultCity="Herzogenaurach" />
+
+        <div>
+          <p className="myGit">
+            This project was created by {""}
+            <a href="https://bukeysblog.wordpress.com/" target="-blank">
+              {" "}
+              Arasanyin Olubukola
+            </a>{" "}
+            and it is{" "}
+            <a
+              href="https://github.com/Bukilola/react-weather-app"
+              target="-blank"
+            >
+              {" "}
+              open-sourced on GitHub
+            </a>
+          </p>
+        </div>
       </div>
     </div>
   );
