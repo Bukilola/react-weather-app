e516f63c5c4d1c2a6a489cbfea1e67c17ee987f3
Bukilola <Arasanyin01@gmail.com>
2023-01-13 16:46:39 +0100

    still working

--- src/Weather.js
+++ src/Weather.js
@@ -1,45 +1,70 @@
-import React from "react"
-import "./Weather.css"
-export default function Weather() {
+import React, { useState } from "react";
+import WeatherInfo from "./WeatherInfo";
+
+import axios from "axios";
+import "./Weather.css";
+
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
+
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      coordinates: response.data.coord,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      date: new Date(response.data.dt * 1000),
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+  function search() {
+    const apiKey = "242999345d2e2boftacbfd08fd72e7b3";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+  if (weatherData.ready) {
     return (
       <div className="Weather">
-        <form>
-            <div className="row">
-          <div className="col-9">
-            <input type="search"
-             placeholder="Enter a city.." 
-             className="form-control"
-            />
-</div>
-<div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" 
-            />
-       </div>
-       </div>
-       </form>
-        <h1>Herzogenaurach</h1>
-
-        <ul>
-          <li>Wednesday 21:59</li>
-          <li>Partly Cloudy</li>
-        </ul>
-        <div className="row">
-          <div className="col-6">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-              alt="Partly Cloudy"
-            />
-            7°C
+         <form onSubmit={handleSubmit}>
+        
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control"
+                autoFocus="on"
+                onChange={handleCityChange}
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
           </div>
-          <div className="col-6">
-            <ul>
-              <li>Precipitation: 5%</li>
-              <li>Humidity: 86%</li>
-              <li>Wind: 16 km/h</li>
-            </ul>
-          </div>
-        </div>
+        </form>
+        <WeatherInfo data={weatherData} />
+       
       </div>
-    
     );
+  } else {
+    search();
+    return "Loading...";
+  }
 }
